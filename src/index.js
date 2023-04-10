6c1848b89d73b2055bb19084f59e06b2ad820770
Prince Dammy <bestbynature2010@gmail.com>
2023-04-10 21:53:12 +0100

    :zap: Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import './mystyles.css';
 import submit from './modules/submit.js';
 
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('button');
 
-const fetch_data = async () => {
+const fetchData = async () => {
   const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6e8u7sd3gV3AU9VxJF2f/scores');
   const data = await response.json();
   return data;
-}
+};
 
 const getFetched = async () => {
-  const values = document.querySelector('.values')
-  const data = await fetch_data();
-  const dataArr = data.result
+  const values = document.querySelector('.values');
+  const data = await fetchData();
+  const dataArr = data.result;
   values.innerHTML = '';
   dataArr.forEach((element) => {
-      values.innerHTML += `<tr><td>${element.user}: ${element.score} </td></tr>`
-    })
-    const trow = document.querySelectorAll('tr')
-    trow.forEach((row, i) => {
-      if(i%2 === 0) row.classList.add('dark')
-    })
-}
-buttons[0].addEventListener('click', getFetched)
-buttons[1].addEventListener('click', submit)
+    values.innerHTML += `<tr><td>${element.user}: ${element.score} </td></tr>`;
+  });
+  const trow = document.querySelectorAll('tr');
+  trow.forEach((row, i) => {
+    if (i % 2 === 0) row.classList.add('dark');
+  });
+};
+buttons[0].addEventListener('click', getFetched);
+buttons[1].addEventListener('click', submit);
--- src/modules/submit.js
+++ src/modules/submit.js
@@ -1,18 +1,18 @@
-import { main } from "./fetch.js"
+import main from './fetch.js';
 
 const submit = () => {
-    const name = document.querySelector('#name')
-    const score = document.querySelector('#score')
-    
-    if(name.value === '' || score.value === '') return
-    else {
-        let pName = name.value;
-        let pScore = score.value;
-        const post = document.querySelector('.post-message')
-        post.innerHTML = 'Loading...'
-        main(pName, pScore)
-        name.value = score.value = '';
-    }
-}
+  const name = document.querySelector('#name');
+  const score = document.querySelector('#score');
 
-export default submit
+  if (name.value === '' || score.value === '') return;
+
+  const pName = name.value;
+  const pScore = score.value;
+  const post = document.querySelector('.post-message');
+  post.innerHTML = 'Loading...';
+  main(pName, pScore);
+  name.value = '';
+  score.value = '';
+};
+
+export default submit;
